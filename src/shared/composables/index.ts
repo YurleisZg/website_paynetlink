d2964c2f5d08250bfcece98c8d7f0943e4edfeb2
Osnaider Miranda <osnaiderluis94@gmail.com>
2026-03-06 23:24:56 -0500

    refactor(navigation): centralize nav links into useNavLinks composable

--- src/shared/composables/index.ts
+++ src/shared/composables/index.ts
@@ -1,4 +1,5 @@
 export { useDebounce } from "./useDebounce";
 export { useLocale } from "./useLocale";
+export { useNavLinks } from "./useNavLinks";
 export { useSearch } from "./useSearch";
 export type { UseSearchOptions, UseSearchReturn } from "./useSearch";
